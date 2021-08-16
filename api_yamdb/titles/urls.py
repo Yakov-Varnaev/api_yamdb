04747dc719632b9e979451a5eb5b3eeae5ebf3ae
Yakov Varnaev <yakov.varnaev@mail.ru>
2021-08-16 13:35:34 +0300

    Genres fully completed.

--- api_yamdb/titles/urls.py
+++ api_yamdb/titles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet
+from .views import CategoryViewSet, GenreViewSet
 
 router = DefaultRouter()
 router.register('categories', CategoryViewSet)
+router.register('genres', GenreViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api_yamdb/titles/views.py
+++ api_yamdb/titles/views.py
@@ -1,9 +1,11 @@
+from django.db.models import query
 from rest_framework import mixins, permissions
 from rest_framework.viewsets import GenericViewSet
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Genre
+from .serializers import CategorySerializer, GenreSerializer
 from .permissions import IsAdminOrReadOnly
+
 
 class RetrieveCreateDeleteViewSet(mixins.ListModelMixin,
                                 mixins.DestroyModelMixin,
@@ -15,6 +17,17 @@ class RetrieveCreateDeleteViewSet(mixins.ListModelMixin,
 class CategoryViewSet(RetrieveCreateDeleteViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    lookup_field = 'slug'
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly
+    ]
+    search_fields = ('name',)
+
+
+class GenreViewSet(RetrieveCreateDeleteViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
     lookup_field = 'slug'
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly,
