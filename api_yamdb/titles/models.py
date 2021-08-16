e11044bf35e8196f3e1edde3ccad618d7695b8fb
Yakov Varnaev <yakov.varnaev@mail.ru>
2021-08-16 13:47:19 +0300

    title model added.

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -23,3 +23,30 @@ class Genre(models.Model):
         max_length=50,
         unique=True
     )
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='title name',
+        max_length=256
+    )
+    year = models.IntegerField(
+        verbose_name='year of issue',
+    )
+    # rating, description, genre, category
+    rating = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    description = models.TextField(
+        verbose_name='title description',
+        max_length=1000,
+    )
+    genre = models.ManyToManyField(
+        to=Genre,
+        related_name='titles',
+    )
+    category = models.ManyToManyField(
+        to=Category,
+        related_name='titles'
+    )
