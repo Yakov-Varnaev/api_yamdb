032ea560516e60ec15a71c02a99fb5669a7f47dd
Yakov Varnaev <yakov.varnaev@mail.ru>
2021-08-16 17:05:11 +0300

    admin panel improvements

--- api_yamdb/titles/admin.py
+++ api_yamdb/titles/admin.py
@@ -5,16 +5,20 @@ from .models import Category, Genre, Title
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug')
-
+    search_fields = ('name', 'slug')
+    empty_display = '--empty--'
 
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug')
+    search_fields = ('name', 'slug')
+    empty_display = '--empty--'
 
 
 class TitleAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'name', 'year', 'rating', 'description',
     )
+    search_fields = ('name', 'year', 'description')
     empty_display = '--empty--'
 
 
