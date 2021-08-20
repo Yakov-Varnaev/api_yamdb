3061eb5b7a5c54b8e974bf694a8fc8f2ccd370b1
Yakov Varnaev <yakov.varnaev@mail.ru>
2021-08-20 15:50:45 +0300

    user admin changes

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -3,10 +3,8 @@ from django.contrib import admin
 from .models import User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'first_name', 'last_name', 'role')
     search_fields = ('username', 'first_name', 'last_name')
     empty_value_display = '--empty--'
-
-
-admin.site.register(User, UserAdmin)
