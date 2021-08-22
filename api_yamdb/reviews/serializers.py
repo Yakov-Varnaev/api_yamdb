52e7793cce98a9a72d380ddffebc7f1ed75a3b01
Yakov Varnaev <yakov.varnaev@mail.ru>
2021-08-22 14:29:03 +0300

    validation is moved back to perform_create

--- api_yamdb/reviews/serializers.py
+++ api_yamdb/reviews/serializers.py
@@ -1,7 +1,6 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
-from .models import Review, Comment, Title
+from .models import Review, Comment
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -15,25 +14,6 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         exclude = ('title',)
-
-    def validate(self, attrs):
-        if self.context['request'].method != 'POST':
-            return super().validate(attrs)
-
-        title = get_object_or_404(
-            Title,
-            pk=self.context['view'].kwargs.get('title_id')
-        )
-
-        if Review.objects.filter(
-            author=self.context['request'].user,
-            title=title
-        ).exists():
-            raise serializers.ValidationError(
-                detail=f'You have already reviewed {title.name}'
-            )
-
-        return super().validate(attrs)
 
 
 class CommentSerializer(serializers.ModelSerializer):
